527b977d9d08d1da3e3b32f4647d2e0a345d0cc8
vakonyakhin <vakonyakhin@gmail.com>
2021-09-27 22:36:39 +0300

    move predict_rub_salary func to general_functions.py add vars moscow_city_id and it_vacancies_catalog_id = 48 reformat get_sj_summary_vacancies func reformat get_sj_vacancies func

--- get_sj_salaries.py
+++ get_sj_salaries.py
@@ -1,55 +1,34 @@
-import os
 import requests
 
-from dotenv import load_dotenv
+from general_functions import predict_rub_salary
 
-def get_sj_vacancies(language):
-    load_dotenv()
-    sj_key = os.getenv('SJ_KEY')
+
+def get_sj_vacancies(language, api_key):
     vacancies_pages = []
     url = 'https://api.superjob.ru/2.0/vacancies'
     header = {
-        'X-Api-App-Id': sj_key,
+        'X-Api-App-Id': api_key,
     }
 
-    page = 0
-    pages_number = 1
-
-    while page < pages_number:
+    moscow_city_id = 4
+    it_vacancies_catalog_id = 48
+    while True:
         params = {
-            'town': 4,
+            'town': moscow_city_id,
             'count': 100,
             'page': 0,
             'keyword': language,
-            'catalogues': 48,
+            'catalogues': it_vacancies_catalog_id,
         }
         response = requests.get(url, params=params, headers=header)
         response.raise_for_status()
         vacancies_pages.append(response.json())
+        more_pages = response.json()['more']
 
-        total = response.json()['total']
-
-        if total <= params['count']:
-            page += 1
-        else:
-            total -= params['count']
-            pages_number += 1
-            page += 1
+        if not more_pages:
+            break
 
     return vacancies_pages
-
-
-#
-#
-def predict_rub_salary(vacancy):
-    if vacancy['currency'] != 'rub':
-        return None
-    elif vacancy['payment_from'] and vacancy['payment_to']:
-        return (vacancy['payment_from'] + vacancy['payment_to']) / 2
-    elif vacancy['payment_from'] and not vacancy['payment_to']:
-        return vacancy['payment_from'] * 1.2
-    elif vacancy['payment_to'] and not vacancy['payment_from']:
-        return vacancy['payment_to'] * 0.8
 
 
 def get_sj_summary_vacancies(vacancies_pages):
@@ -61,9 +40,10 @@ def get_sj_summary_vacancies(vacancies_pages):
         vacancies_found = vacancies_page['total']
 
     for vacancy in vacancies_list:
-        salary = predict_rub_salary(vacancy)
-        if salary:
-            salaries.append(salary)
+        if vacancy['currency'] == 'rub':
+            salary = predict_rub_salary(vacancy['payment_from'], vacancy['payment_to'])
+            if salary:
+                salaries.append(salary)
     try:
         average_salary = int((sum(salaries) / len(salaries)))
     except ZeroDivisionError:
